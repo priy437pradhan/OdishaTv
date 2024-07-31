4be6d9d85790326c43024c4b84f640adda16ea97
Priya Pradhan <priy437pradhan@gmail.com>
2024-07-31 10:58:34 +0530

    new header design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,31 +7,28 @@ import LiveSubCard from '../components/LiveSubCard';
 
 const NewsList = ({
   liveNewsItem = { imageUrl: testing, headline: 'Live News Headline' },
-  singleCardItem =[
-    {id:1, imageUrl: testing, headline: 'Single News Headline' },
-    {id:2, imageUrl: testing, headline: 'Single News Headline 2' },
-    {id:3, imageUrl: testing, headline: 'Single News Headline 3' },
-    {id:4, imageUrl: testing, headline: 'Single News Headline 4' },
-    {id:5, imageUrl: testing, headline: 'Single News Headline 5' },
+  singleCardItem = [
+    { id: 1, imageUrl: testing, headline: 'Single News Headline 1' },
+    { id: 2, imageUrl: testing, headline: 'Single News Headline 2' },
+    { id: 3, imageUrl: testing, headline: 'Single News Headline 3' },
+    { id: 4, imageUrl: testing, headline: 'Single News Headline 4' },
+    { id: 5, imageUrl: testing, headline: 'Single News Headline 5' },
   ],
-  liveSubCardItem =[
-    { id: 11,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 12,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 14,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 15,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 16,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 17,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 18,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-    { id: 19,imgSrc: testing, title: 'Live Subcard News Headline', content: 'xxxxxxxxx' },
-  ]
+  liveSubCardItem = [
+    { id: 11, imgSrc: testing, title: 'Live Subcard News Headline 1', content: 'xxxxxxxxx' },
+    { id: 12, imgSrc: testing, title: 'Live Subcard News Headline 2', content: 'xxxxxxxxx' },
+    { id: 14, imgSrc: testing, title: 'Live Subcard News Headline 3', content: 'xxxxxxxxx' },
+    { id: 15, imgSrc: testing, title: 'Live Subcard News Headline 4', content: 'xxxxxxxxx' },
+    { id: 16, imgSrc: testing, title: 'Live Subcard News Headline 5', content: 'xxxxxxxxx' },
+    { id: 17, imgSrc: testing, title: 'Live Subcard News Headline 6', content: 'xxxxxxxxx' },
+    { id: 18, imgSrc: testing, title: 'Live Subcard News Headline 7', content: 'xxxxxxxxx' },
+    { id: 19, imgSrc: testing, title: 'Live Subcard News Headline 8', content: 'xxxxxxxxx' },
+  ],
 }) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[3fr_6fr_3fr] gap-6 mt-12 lg:mt-56 px-4 lg:px-0">
-
-
-
-<div className="lg:col-span-1">
-        {singleCardItem.map((item, index) => (
+      <div className="lg:col-span-1">
+        {singleCardItem.map((item) => (
           <Link key={item.id} to={`/story/${item.id}`} className="overflow-hidden block mb-2">
             <SingleCard imageUrl={item.imageUrl} headline={item.headline} />
           </Link>
@@ -42,9 +39,8 @@ const NewsList = ({
           <Link to="/story/live" className="bg-white shadow-md rounded-sm overflow-hidden block mb-2">
             <LiveNewsCard imageUrl={liveNewsItem.imageUrl} headline={liveNewsItem.headline} />
           </Link>
-
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-            {liveSubCardItem.map((item, index) => (
+            {liveSubCardItem.map((item) => (
               <Link key={item.id} to={`/story/${item.id}`} className="overflow-hidden block mb-2">
                 <LiveSubCard title={item.title} content={item.content} imgSrc={item.imgSrc} />
               </Link>
@@ -52,9 +48,6 @@ const NewsList = ({
           </div>
         </div>
       </div>
-
-      
-
       <div className="lg:col-span-1 bg-gray-100 p-4">
         <h2 className="text-lg font-semibold mb-4">Placeholder Column 3</h2>
         <p>Content or naming for column 3 goes here.</p>
